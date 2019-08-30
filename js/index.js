4905268df1182f42ec5c990b6c1d3fab34fad615
Sebastian Glaser <anx@ulzq.de>
2019-08-30 10:12:39 +0200

    add: hotfix for presentation

--- js/index.js
+++ js/index.js
@@ -1,44 +1,48 @@
 
-var hashChange = function(){
+function hashChange(){
   var active = document.querySelector('.active')
   if ( active ){
     active.classList.remove('active')
   }
   var newItem = document.querySelector(`a.navi[href="${window.location.hash}"]`)
-  if ( newItem ){
-    newItem.classList.add('active')
-  } else {
-    document.querySelector(`a.navi[href="#history"]`)
+  if ( !newItem ){
+    newItem = document.querySelector(`a.navi[href="#history"]`)
   }
+  newItem.classList.add('active')
+}
+
+function nextSlide() {
+  var list   = Array.from(document.querySelectorAll("a.navi"))
+  var active = document.querySelector("a.navi.active")
+  var index  = list.indexOf(active)
+  var length = list.length
+  var prev = ( index + 1 + length ) % length
+  var skipTo = list[prev]
+  window.location = skipTo.href
+}
+
+function prevSlide() {
+  if ( window.location.hash.match(/job/) ){
+    window.location = '#history'
+    return
+  }
+  var list   = Array.from(document.querySelectorAll("a.navi"))
+  var active = document.querySelector("a.navi.active")
+  var index  = list.indexOf(active)
+  var length = list.length
+  var prev = ( index - 1 + length ) % length
+  var skipTo = list[prev]
+  window.location = skipTo.href
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-
   if ( ! window.location.toString().match('#') ){
     window.location = window.location + '#overview'
-  } else {
-    hashChange()
-  }
-
+  } else hashChange()
   window.addEventListener('hashchange',hashChange)
-
-  document.querySelector('.prev').addEventListener('click',()=>{
-    var list   = Array.from(document.querySelectorAll("a.navi"))
-    var active = document.querySelector("a.navi.active")
-    var index  = list.indexOf(active)
-    var length = list.length
-    var prev = ( index - 1 + length ) % length
-    var skipTo = list[prev]
-    window.location = skipTo.href
-  })
-
-  document.querySelector('.next').addEventListener('click',()=>{
-    var list   = Array.from(document.querySelectorAll("a.navi"))
-    var active = document.querySelector("a.navi.active")
-    var index  = list.indexOf(active)
-    var length = list.length
-    var prev = ( index + 1 + length ) % length
-    var skipTo = list[prev]
-    window.location = skipTo.href
+  document.querySelector('.prev').addEventListener('click',prevSlide)
+  document.querySelector('.next').addEventListener('click',nextSlide)
+  document.addEventListener('keydown',(e)=>{
+    if (e.key == 'space') nextSlide()
   })
 })
